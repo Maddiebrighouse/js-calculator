8a0fec8b9f72244cff25d3de077f31e195f0bb2c
madeleine-brighouse <31881309+Maddiebrighouse@users.noreply.github.com>
2017-12-29 22:01:43 +1300

    hfjkdhfjkdshjkl

--- calculator.js
+++ calculator.js
@@ -1,11 +1,16 @@
-let display = ""
-let memory = ""
+
+window.onload = function (){
+document.getElementById("0").addEventListener("click", display);
+}
+let display = [];
+let equation = [];
 
 // Show equation on screen
 function buttonFunction(){
-document.getElementById().value.innerHTML.output
+  let display = "help im a fish"
+  document.getElementById("entry").innerHTML = display;
 }
-}
+
 
 
 //clear entry
